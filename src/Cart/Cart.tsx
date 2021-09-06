c8de4f5922c6686fa8e5214ee680d247880b2965
Milosz Mazurkiewicz <miloszmazurk@gmail.com>
2021-09-06 15:54:25 +0200

    Added total amount in cart

--- src/Cart/Cart.tsx
+++ src/Cart/Cart.tsx
@@ -11,6 +11,13 @@ type Props = {
 };
 
 const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) => {
+    return items.reduce(
+      (ack: number, item) => ack + item.amount * item.price,
+      0
+    );
+  };
+
   return (
     <Wrapper>
       <h2>Your Shopping Cart</h2>
@@ -23,6 +30,7 @@ const Cart: React.FC<Props> = ({ cartItems, addToCart, removeFromCart }) => {
           removeFromCart={removeFromCart}
         />
       ))}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 };
